ad222934f3f9decab5402d41fedbcc256c1f047f
Polarstingray <polarstingray7@gmail.com>
2026-05-25 12:03:12 -0500

    -add in Meal object, foods is occurence dictionary, updated all helpers to utilize new self.foods

--- backend/src/foods.py
+++ backend/src/foods.py
@@ -1,3 +1,5 @@
+import sys
+
 '''
 food and meal objects here
 
@@ -17,31 +19,75 @@ class Food:
         self.desc = desc
         self.pic = pic
 
-
 class Meal:
     def __init__(self, name, foods, stores={}, desc="", pic=None):
         self.name = name
-        self.foods = foods
         self.desc = desc
         self.pic = pic
 
+        self.foods = {}
+        if type(foods) == list :
+            self.add_foods(foods)
+        elif type(foods) == Food :
+            self.foods = {foods : 1}
+        else :
+            self.foods = foods
 
     def get_price(self) :
         total = 0.0
-        for food in self.foods :
-            total+= food.cost
+        for food, amount in self.foods.items() :
+            total += food.cost * amount
         return total
+
+    def add_food(self, food, amount=1) :
+        if not food :
+            print("Must add a food with a name", file=sys.stderr)
+            return -1
+        if (amount < 1) :
+            print("Invalid amount to add", file=sys.stderr)
+            return -1
+        
+        if food in self.foods :
+            self.foods[food] += amount
+        else :
+            self.foods[food] = amount
+
+    # add a list of foods, mostly just for testing 
+    def add_foods(self, foods) :
+        for food in foods :
+            self.add_food(food)
+
+    def remove_food(self, food, amount=1) :
+        if not food :
+            print("Must remove a food with a name", file=sys.stderr)
+            return -1
+        if (amount < 1) :
+            print("Invalid amount to remove", file=sys.stderr)
+            return -1
+        
+        if food in self.foods :
+            if self.foods[food] < amount :
+                self.foods[food] = 0
+            else :
+                self.foods[food] -= amount
+        else :
+            print("Food item not in meal", file=sys.stderr)
+            return -1
+        if self.foods[food] == 0 :
+            self.foods.pop(food, None)
+            print("Removing food item from ingredient list")
+        return 0
 
     def get_macros(self) :
         cals = 0
         carbs = 0.0
         prot = 0.0
         fat = 0.0
-        for food in self.foods :
-            cals+= food.cals
-            carbs+= food.carbs
-            prot+= food.protein
-            fat+= food.fat
+        for food, amount in self.foods.items() :
+            cals+= food.cals * amount
+            carbs+= food.carbs * amount
+            prot+= food.protein * amount
+            fat+= food.fat * amount
         return {
             "calories" : cals,
             "carbs" : carbs,
@@ -57,7 +103,8 @@ def main() :
 
     print(apple_pie.get_macros())
     print(apple_pie.get_price())
+    print(apple_pie.foods)
 
 
-
-main()
+if __name__ == "__main__" :
+    main()
