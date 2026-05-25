a8549b3557fad9c86e2c2fbd9dd0abcd51928ae1
Polarstingray <polarstingray7@gmail.com>
2026-05-25 15:12:17 -0500

    -add unique_list helper for returning unique sets of meals or foods -add add_to_db and rm_from_db, add/remove meals/foods from pickle db

--- backend/src/grocery.py
+++ backend/src/grocery.py
@@ -27,31 +27,102 @@ def write_db(data, db=FOOD_AND_MEALS) :
     with open(db, 'wb') as f:
         pickle.dump(data, f)
 
+def unique_list(dat1, dat2) :
+    names = []
+    unique_objects = []
+    for obj in (dat1 + dat2) :
+        if obj.name not in names :
+            names.append(obj.name)
+            unique_objects.append(obj)
+    return unique_objects
+
+def add_to_db(data, db=FOOD_AND_MEALS) :
+    if len(data) > 1 :
+        curr_data = read_db(db)
+        if (not curr_data) :
+            write_db(list(set(data[0])), list(set(data[1])))
+        if (len(curr_data) > 1) :
+            unique_foods = unique_list(curr_data[0], data[0])
+            unique_meals = unique_list(curr_data[1], data[1])
+            write_db([unique_foods, unique_meals], db)
+            return 0
+        else :
+            print("error, corrupted db")
+            return -1
+    
+    return -1
+
 def read_db(db=FOOD_AND_MEALS) :
     with open(db, "rb") as f:
         data =pickle.load(f)
     return data
+
+def rm_from_db(name, db=FOOD_AND_MEALS) :
+    foods, meals = read_db(db)
+
+    for i in range(len(foods)) :
+        if foods[i].name == name :
+            foods.pop(i)
+            write_db((foods, meals))
+            return 0
+
+    for i in range(len(meals)) :
+        if meals[i].name == name :
+            meals.pop(i)
+            write_db((foods, meals))
+            return 0
+        
+    print("Error, item not found")
+    return 1
+
+        
+
+
+
 
 def main() :
     apple = Food("apple", {"cub, target, walmart, co-op"}, 2.0, 40, 2, 0, 1, "Honeycrisp apple")
     crust = Food("pie crust", "cub, target", 3.99, 100, 20, 4, 5, "Sweet-Butter pie crust")
 
     apple_pie = Meal("apple pie", [apple, apple, apple, crust], {}, "an apple pie")
-
-    print(apple_pie.get_macros())
-    print(apple_pie.get_price())
-    for food in apple_pie.foods :
-        print(f"{food} x{apple_pie.foods[food]}")
+    
+    print("===================")
+    print("reading db\n")
 
     foods = [apple, crust]
     meals = [apple_pie]
 
-    write_db([foods, meals])
+    # write_db([foods, meals])
+    add_to_db([foods, meals])
 
-    data = read_db()
-    for item in data :
-        for food_meal in item :
-            print(food_meal)
+    loaded_data = read_db()
+    for item in loaded_data :
+        for ele in item :
+            print(ele)
+
+    rice = Food("rice", {"cub, target, walmart, co-op"}, 13.99, 100, 30, 5, 1, "jasmine rice")
+    pizza_crust = Food("pie crust", "cub, target", 3.99, 100, 20, 3, 2, "pizza dough")
+
+    mystery_meal = Meal("nonsense", [rice, apple, pizza_crust, crust], {}, "an apple pie")
+
+    new_foods = loaded_data[0]
+    new_meals = loaded_data[1]
+
+    new_foods.append(rice)
+    new_foods.append(pizza_crust)
+    new_meals.append(mystery_meal)
+
+    print("adding new items to db")
+    add_to_db([new_foods, new_meals])
+
+    rm_from_db("apple")
+
+    print("=============================")
+    print("reading db\n")
+    for item in read_db() :
+        for ele in item :
+            print(ele)
+
 
 
 if __name__ == "__main__" :
